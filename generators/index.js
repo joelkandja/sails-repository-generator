e20d8659ff6bf916e67ebadd31add934c2579878
joel kandja <joelkandja04@gmail.com>
2025-07-20 13:27:35 +0100

    fix error

--- generators/index.js
+++ generators/index.js
@@ -1,42 +1,32 @@
 module.exports = {
+    friendlyName: 'Generate resource',
 
-    /**
-     * Custom generator for sails
-     */
-    friendlyName: 'Generate Repository + Service',
-
-    description: 'Generate Repository and Service files',
+    description: 'Generate a repository and a service for a model',
 
     inputs: {
         name: {
-            description: 'The name of the resource',
-            required: true,
-            type: 'string'
+            description: 'The name of the resource (e.g. user)',
+            example: 'user',
+            required: true
         }
     },
 
     fn: async function (inputs, exits) {
-        const { name } = inputs;
         const _ = require('lodash');
-        const fs = require('fs');
         const path = require('path');
 
-        const resourceName = _.upperFirst(_.camelCase(name));
+        const name = _.kebabCase(inputs.name);
+        const capitalized = _.upperFirst(_.camelCase(inputs.name));
 
-        const basePath = path.resolve('api');
-        const repoPath = path.join(basePath, 'repositories');
-        const servicePath = path.join(basePath, 'services');
+        await this.helpers.template.generate({
+            templateDir: path.resolve(__dirname, 'templates'),
+            targetDir: path.resolve('api'),
+            data: {
+                name: capitalized,
+                nameLower: inputs.name.toLowerCase()
+            }
+        });
 
-        if (!fs.existsSync(repoPath)) fs.mkdirSync(repoPath);
-        if (!fs.existsSync(servicePath)) fs.mkdirSync(servicePath);
-
-        const templateRepo = fs.readFileSync(path.join(__dirname, 'templates/Repository.js'), 'utf8');
-        const templateService = fs.readFileSync(path.join(__dirname, 'templates/Service.js'), 'utf8');
-
-        fs.writeFileSync(path.join(repoPath, `${resourceName}Repository.js`), templateRepo.replace(/<%= name %>/g, resourceName));
-        fs.writeFileSync(path.join(servicePath, `${resourceName}Service.js`), templateService.replace(/<%= name %>/g, resourceName));
-
-        sails.log.info(`✔️ ${resourceName}Repository & ${resourceName}Service generated.`);
         return exits.success();
     }
 };
